b4001b9e8262462a841b8d5c6f56a1ae818adb45
MosheKP2022 <mperry7@chicagobooth.edu>
2022-02-09 22:26:24 +0000

    youngest and eldest working

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
 
   get("/directors", { :controller=> "director", :action=> "index" })
+
+  get("/directors/eldest", { :controller=> "director", :action=> "wisest" })
+
+  get("/directors/youngest", { :controller=> "director", :action=> "dumbest" })
 end
